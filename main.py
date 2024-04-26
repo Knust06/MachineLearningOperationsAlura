3f9479cb78125ca813fc9c83d62f0daa6338f2ae
Knust06 <lucasknust0605@gmail.com>
2024-04-26 10:48:10 -0300

    Adicionando modelo, jupyter notebook para testar e autenticação básica.

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
 from textblob import TextBlob
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import pickle
 
-url = 'https://raw.githubusercontent.com/alura-cursos/1576-mlops-machine-learning/aula-5/casas.csv'
-df = pd.read_csv(url)
-colunas = ['tamanho', 'ano','garagem']
+colunas = ['tamanho', 'ano', 'garagem']
+modelo = pickle.load(open('modelo.sav', 'rb'))
 
-df.head()
-x = df.drop('preco', axis = 1)
-y = df['preco']
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state=42)
-modelo = LinearRegression()
-modelo.fit(x_train, y_train)
 
 app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'lucas'
+app.config['BASIC_AUTH_PASSWORD'] = 'guts'
 
-
+basic_auth = BasicAuth(app)
 
 @app.route('/')
 def home():
@@ -25,14 +20,16 @@ def home():
 
 
 @app.route('/sentimento/<frase>')
+@basic_auth.required
 def sentimento(frase):
-    translator = Translator()
+    #translator = Translator()
     tb = TextBlob(frase)
     #tb_en = tb.translate(from_lang='pt_br', to='en')
     polaridade = tb.sentiment.polarity
     return f'Polaridade: {polaridade}'
 
 @app.route('/cotacao/',methods=['POST'])
+@basic_auth.required
 def cotacao():
     dados = request.get_json()
     dados_input = [dados[col] for col in colunas]
