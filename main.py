fb8db39bd2c7329c4f6266f8a12442f8d645582c
Knust06 <lucasknust0605@gmail.com>
2024-04-26 09:28:09 -0300

    Adicionando analise de sentimento

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from textblob import TextBlob
+from googletrans import Translator
 
-app = Flask('meu_app')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Olá, mundo!'
+
+
+@app.route('/sentimento/<frase>')
+def sentimento(frase):
+    translator = Translator()
+    tb = TextBlob(frase)
+    #tb_en = tb.translate(from_lang='pt_br', to='en')
+    polaridade = tb.sentiment.polarity
+    return f'Polaridade: {polaridade}'
+
+app.run(debug=True)
